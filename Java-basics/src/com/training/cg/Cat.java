de57dfc5d0c2ae18d18d18af1c3a4f9752f00931
mkumar12 <abc@mail.com>
2022-03-08 12:35:25 +0530

    add

--- Java-basics/src/com/training/cg/Cat.java
+++ Java-basics/src/com/training/cg/Cat.java
@@ -13,3 +13,32 @@ public class Cat extends Animal {
 	 */
 
 }
+
+// We want a simple banking application
+
+// Create an Account interface with following methods
+// deposit(double amount)
+// withdraw(double amount)
+// checkBalance()
+
+//2
+// create a class "SavingAccount" which will implement the "Account" interface
+// give implementation to the overridden methods
+// create a constant as initialBalance
+// create a variable as totalBalance
+//create a variable of rateOfInterest --> 5%
+
+//3
+
+//create a class "CurrentAccount" which will implement the "Account" interface
+//give implementation to the overridden methods
+//create a constant as initialBalance
+//create a variable as totalBalance 
+// create a variable of rateOfInterest -->4
+
+// Create a class "AccountType" with the common code in SavingAccount and
+// CurrentAccount and make it a parent class for SavingAccount and CurrentAccount
+
+//4
+// create a method "getInterest(int timePeriod)" and then calculate the interest based
+// on rate of interest.
